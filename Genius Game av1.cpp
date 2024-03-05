67d32600df125aa80998e641a11c2c0d743c558d
HenrSF <94074229+HenrSF@users.noreply.github.com>
2024-03-05 19:04:29 -0300

    Update Genius Game av1.cpp

--- Genius Game av1.cpp
+++ Genius Game av1.cpp
@@ -2,18 +2,15 @@
 
   int pushbutton[] = {7, 6, 5, 4};
   int led_pin[] = {12, 11, 10, 9};
-  int sequence_length = 0;
   int generated_sequence[100];
-  int current_round = 0;
-  int max_rounds = 2;
 
-  //unsigned long button_time_limit = 3000;
-  //unsigned long game_time = 0;
+  int sequence_length = 0;
+  int current_round = 0;
+  int max_rounds = 7; //rounds to win
 
   bool game_running = true;
   
   void setup() {
-    //defining leds and button pins
     Serial.begin(9600);
     randomSeed(analogRead(0));
     for (int n = 0; n < 4; n++) {
@@ -23,6 +20,7 @@
   }
 
   void generate_sequence(){
+  //generate the sequence which the user has to match
     
     for (int i = 0; i < sequence_length; i++) {
       digitalWrite(led_pin[generated_sequence[i]], HIGH);
@@ -31,48 +29,32 @@
       delay(400);
       Serial.print(led_pin[generated_sequence[i]]);
     }
-    //it can't put a led immediatly after the sequence
-    /*generated_sequence[sequence_length] = led_pin[random(0,4)];
-    sequence_length++;
-    delay(100);
-    */
   }
-  /*
-    void wait_input(){
-    for(int i = 0; i < sequence_length; i++){
-    input();
-                      
-    }
-    }
-  */
   int input() {
-    //game_time = millis();
     //read user inputs, aka, buttons pressed.
+    
     while (true) {
       for (int i = 0; i < 4; i++) {
         int pushb_pin = digitalRead(pushbutton[i]);
         if (pushb_pin == LOW) {
           Serial.print(i);
           return i; //return the pressed button pin.
-
         }
       }
-      delay(1);
     }
-
   }
+
   void light_leds(int led_pin_number) {
+    //when pressed a button, light the corresponding led to that button
+    
     digitalWrite(led_pin[led_pin_number], HIGH);
     delay(300);
     digitalWrite(led_pin[led_pin_number], LOW);
   }
 
-  //void play_user_sequence(){}
-
   bool check_sequence() {
-    /*if(millis() - game_time > button_time_limit){
-    	game_over();
-    }*/
+    //see if the input from the player is identical to the program generate sequence
+    
     for (int i = 0; i < sequence_length; i++) {
       int game_sequence = generated_sequence[i];
       int pressed_button = input();
@@ -85,30 +67,32 @@
   }
 
   void game_over() {
-    //flash the red LED, indicating that the user's input didn't match with the generated sequence.
+    //light all leds if the player has the wrong input and resets the sequence
+    
     sequence_length = 0;
     for(int i =0; i < 4; i++){
     digitalWrite(led_pin[i], HIGH);
     }
-    /*digitalWrite(12, HIGH);
-    digitalWrite(11, HIGH);
-    digitalWrite(10, HIGH);
-    digitalWrite(9, HIGH);*/
   }
   void won(){
+    //if the player manages to win the game then all the leds will first light, from red to blue in sequence
+    //after that, all leds will be turned on
+    
     for (int i = 0; i < 4; i++) {
-    digitalWrite(led_pin[i], HIGH); // Turn on the current LED
+    digitalWrite(led_pin[i], HIGH);
     delay(200); // Delay to keep the LED on
-    digitalWrite(led_pin[i], LOW); // Turn off the current LED
-    delay(50); // Delay before turning on the next LED
+    digitalWrite(led_pin[i], LOW);
+    delay(50);
   }
-
-  // Turn on all LEDs simultaneously
   for (int i = 0; i < 4; i++) {
-    digitalWrite(led_pin[i], HIGH); // Turn on all LEDs
+    digitalWrite(led_pin[i], HIGH);
   }
 }
   void loop() {
+    //first checks if the game is running and if the current round is less than max number of rounds
+    //after each iteration, a new led is placed at the end of the sequence
+    //if the user has the wrong input or reached the maximum round, game_running is turned to false and the game stops needing to be reset
+    
     if (game_running && current_round < max_rounds) {
       generated_sequence[sequence_length] = random(0, 4);
       sequence_length++;
@@ -118,14 +102,12 @@
       if (!check_sequence()) {
         game_running = false;
         game_over();
-
       }
       if(current_round == max_rounds){
         game_running = false;
         won();
       }
       delay(500);
-
     }
     delay(1000);
-  } //loop func            
+  }           
